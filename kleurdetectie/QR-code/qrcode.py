fc39e9494879c2806b9a818f04923cd18000ecc7
Matthino868 <matthino868@gmail.com>
2022-12-02 15:01:53 +0100

    qrcode testcode
    Co-authored-by: MeIorito <MeIorito@users.noreply.github.com>
    Co-authored-by: LucasSchreij <LucasSchreij@users.noreply.github.com>

--- kleurdetectie/QR-code/qrcode.py
+++ kleurdetectie/QR-code/qrcode.py
@@ -1,26 +1,76 @@
 import cv2
-img=cv2.imread("C:/Users/arthu/Pictures/QRcodes/BetaQR.png")
-det=cv2.QRCodeDetector()
-retval, decoded_info, points, straight_qrcode = det.detectAndDecodeMulti(img)
-print(retval)
-print(decoded_info)
-print(points)
+img=cv2.imread("C:/Users/arthu/Pictures/QRcodes/eigenqr2.png")
+# cap = cv2.VideoCapture("C:/Users/arthu/Downloads/Instructievideo.mp4")
+# cap = cv2.VideoCapture(1)
 
-for i in points[0]:
-    # print(type(i))
-    # print(i[0])
-    # x, y = i[0] , i[1]
-    # print(x,  y)
-    cv2.circle(img, (int(i[0]), int(i[1])), 3, (255, 0, 255), 5)
+# cap.set(3,1280)
+# cap.set(4,720)
+
+# while cap.isOpened():
+while True:
+    # ret, img = cap.read()
+    # if ret == False:
+    #     continue
+    img=cv2.imread("C:/Users/arthu/Pictures/QRcodes/eigenqr2.png")
+
+    img = cv2.resize(img, (720,720))
+    # print(points)
+    det=cv2.QRCodeDetector()
+    retval, decoded_info, points, straight_qrcode = det.detectAndDecodeMulti(img)
+    # print(points)
+    if retval:
+        print(decoded_info)
+        print(straight_qrcode)
+        for i in points:
+            print(i)
+            # print(i[0])
+            cv2.rectangle(img, (int(i[0][0]),int(i[0][1])), (int(i[2][0]),int(i[2][1])), (255,0,0), 2) # punt
+                
+        # print(points[0][3])
+        # print(points[0][0][0])
+        # cv2.rectangle(img, (int(points[0][0][0]),int(points[0][0][1])), (int(points[0][2][0]),int(points[0][2][1])), (255,0,0), 2) # punt
+
+        # for i in points[0]:
+            # print(type(i))
+            # print(i[0])
+            # x, y = i[0] , i[1]
+            # print(x,  y)
+            # cv2.rectangle(img, (i[0], puntY-puntH//2), (puntX+puntW//2, puntY+puntH//2), colorPunt, 2) # punt
+            # cv2.rectangle(img, (int(points[0][0][0]),int(points[0][0][1])), (int(points[0][2][0]),int(points[0][2][1])), (255,0,0), 2) # punt
+
+            # cv2.circle(img, (int(i[0]), int(i[1])), 3, (255, 0, 255), 5)
+
+    cv2.imshow("Image", img)
+    
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        cap.release()
+        cv2.destroyAllWindows()
+        break
+cap.release()
+cv2.destroyAllWindows()
+
+# det=cv2.QRCodeDetector()
+# retval, decoded_info, points, straight_qrcode = det.detectAndDecodeMulti(img)
+# print(retval)
+# print(decoded_info)
+# print(points)
+
+# for i in points[0]:
+#     # print(type(i))
+#     # print(i[0])
+#     # x, y = i[0] , i[1]
+#     # print(x,  y)
+#     cv2.circle(img, (int(i[0]), int(i[1])), 3, (255, 0, 255), 5)
     
 
 
-cv2.imshow("image", img)
-k = cv2.waitKey(10000) & 0xFF
-# if k == 27:
-#     cap.release()
-#     cv2.destroyAllWindows()
-#     break
-# cap.release()
-cv2.destroyAllWindows()
-# print(straight_qrcode)
+# cv2.imshow("image", img)
+# k = cv2.waitKey(10000) & 0xFF
+# # if k == 27:
+# #     cap.release()
+# #     cv2.destroyAllWindows()
+# #     break
+# # cap.release()
+# cv2.destroyAllWindows()
+# # print(straight_qrcode)
