fe60ad17199c5f610cd29d79fea9a77b41e07b82
Matthino868 <matthino868@gmail.com>
2022-12-05 22:39:26 +0100

    .

--- kleurdetectie/QR-code/pybooftest.py
+++ kleurdetectie/QR-code/pybooftest.py
@@ -4,16 +4,23 @@ import cv2
 
 # Detects all the QR Codes in the image and prints their message and location
 data_path = "C:/Users/arthu/Pictures/QRcodes/eigenqr2.png"
-img=cv2.imread("C:/Users/arthu/Pictures/QRcodes/eigenqr2.png")
+# img = cv2.imread("C:/Users/arthu/Pictures/QRcodes/eigenqr2.png")
 
 
 detector = pb.FactoryFiducial(np.uint8).microqr()
 
-# detector = pb.QrCodeDetector()
+# detector = pb1.QrCodeDetector()
 
-image = pb.load_single_band(data_path, np.uint8)
 
-detector.detect(image)
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, img = cap.read()
+    if ret == False:
+        continue
+    # image = pb.load_single_band(img, np.uint8)
+    detector.detect(img)
+
+
 
 print("Detected a total of {} QR Codes".format(len(detector.detections)))
 
@@ -25,10 +32,11 @@ print(type(qr.bounds))
 bounds = qr.bounds.convert_tuple()
 print(bounds)
 
-cv2.rectangle(img, (int(bounds[0][0]),int(bounds[0][1])), (int(bounds[2][0]),int(bounds[2][1])), (255,0,0), 5) # punt
+cv2.rectangle(img, (int(bounds[0][0]), int(bounds[0][1])), (int(
+    bounds[2][0]), int(bounds[2][1])), (255, 0, 0), 5)  # punt
 
 
-img = cv2.resize(img,(720,720))
+img = cv2.resize(img, (720, 720))
 cv2.imshow("image", img)
 
 k = cv2.waitKey(10000) & 0xFF
--- kleurdetectie/QR-code/qrcode.py
+++ kleurdetectie/QR-code/qrcode.py
@@ -1,5 +1,5 @@
 import cv2
-img=cv2.imread("C:/Users/arthu/Pictures/QRcodes/eigenqr2.png")
+img=cv2.imread("C:/Users/arthu/Pictures/QRcodes/eigenqr3.png")
 # cap = cv2.VideoCapture("C:/Users/arthu/Downloads/Instructievideo.mp4")
 # cap = cv2.VideoCapture(1)
 
@@ -11,19 +11,20 @@ while True:
     # ret, img = cap.read()
     # if ret == False:
     #     continue
-    img=cv2.imread("C:/Users/arthu/Pictures/QRcodes/eigenqr2.png")
+    img=cv2.imread("C:/Users/arthu/Pictures/QRcodes/eigenqr3.png")
 
-    img = cv2.resize(img, (720,720))
+    # img = cv2.resize(img, (720,720))
     # print(points)
     det=cv2.QRCodeDetector()
     retval, decoded_info, points, straight_qrcode = det.detectAndDecodeMulti(img)
     # print(points)
     if retval:
         print(decoded_info)
-        print(straight_qrcode)
-        for i in points:
+        # print(straight_qrcode)
+        for idlijst, i in enumerate(points):
             print(i)
             # print(i[0])
+            cv2.putText(img, decoded_info[idlijst], (int(i[0][0]),int(i[0][1])), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2, cv2.LINE_AA, False)
             cv2.rectangle(img, (int(i[0][0]),int(i[0][1])), (int(i[2][0]),int(i[2][1])), (255,0,0), 2) # punt
                 
         # print(points[0][3])
